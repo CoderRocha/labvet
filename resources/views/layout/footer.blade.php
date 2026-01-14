ddd4ebed2317fea70bc0413fc8a8103e82afa29e
Guilherme <guilhermerochas0420@gmail.com>
2026-01-13 21:43:27 -0300

    feat: add datatable script

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,1 +1,19 @@
-<h1>Footer</h1>
+<script>
+    $(function() {
+            $('#tb_default').DataTable({
+                "paging": true,
+                "lenghtChange": true,
+                "searching": true,
+                "ordering": true,
+                "info": true,
+                "autoWidth": false,
+                "responsive": true,
+                "language": {
+                    "url": "//cdn.datatables.net/plug-ins/1.10.20/i18n/Portuguese-Brasil.json"
+                }
+
+            });
+        }
+
+    )
+</script>
