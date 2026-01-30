1941287b5c98b0b565166d3e9d51e4d603fabf7e
Guilherme <guilhermerochas0420@gmail.com>
2026-01-29 21:07:39 -0300

    refact: ajustado main page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,6 +2,15 @@
 
 @section('body')
 
-<h1>Labvet</h1>
+    <div class="alert alert-primary text-center">
+        <h2>Bem vindo ao LabVet!</h2>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <img class="mx-auto d-block" src="{{ url('/main-image.jpg') }}" style="max-width: 95%;" alt="LabVet Main Image">
+            </div>
+        </div>
+    </div>
 
 @endsection
